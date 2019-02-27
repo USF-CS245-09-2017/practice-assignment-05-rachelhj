44797b8f4058d0fd47d928bffc744e8432eecc64
rachelhj <35712263+rachelhj@users.noreply.github.com>
2019-02-26 22:43:44 -0800

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -8,30 +8,30 @@ public class MergeSort implements SortingAlgorithm{
             sort(left);
             sort(right);
             merge(a,left,right);
-		}
+	}
     }
     private void merge(int[] a, int[] left, int[] right){
 	int ai=0;
 	int left_i = 0;
 	int right_i = 0;
-		while(left_i<left.length && right_i<right.length){
+	    while(left_i<left.length && right_i<right.length){
         	if(left[left_i]<=right[right_i]){
-            	a[ai]=left[left_i];
-            	ai++;
-            	left_i++;
+            	    a[ai]=left[left_i];
+            	    ai++;
+            	    left_i++;
         	}
         	else{
-            	a[ai]=right[right_i];
-            	ai++;
-            	right_i++;
+            	    a[ai]=right[right_i];
+            	    ai++;
+            	    right_i++;
         	}
-		}
-		while(left_i<left.length){
+	    }
+	    while(left_i<left.length){
         	a[ai++]=left[left_i++];
-		}
-		while(right_i<right.length){
+	    }
+	    while(right_i<right.length){
         	a[ai++]=right[right_i++];
-		}
+	    }
+        }
     }
-}
 
